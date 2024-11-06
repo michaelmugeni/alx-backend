ac8bd011b6d50739861a918a6c92f221ebce347c
michaelmugeni <michaelmugeni98@gmail.com>
2024-11-06 14:31:12 +0000

    all added

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,24 +4,27 @@
 from flask_babel import Babel
 from flask import Flask, render_template
 
+
 class Config:
     """Represents a Flask Babel configuration.
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT-TIMEZONE = "UTC"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
-    app =Flask(__name__)
-    app.config.from_object(Config)
-    app.url_map.strict_slashes = False
-    babel = babel(app)
 
-    @app.route('/')
-    def get_index() -> str:
-        """ The home/index page.
-        """
-        return render template('1-index.html')
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
 
-    if __name__ = "__main__":
-        app.run(host='0.0.0.0', port=5000)
 
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
